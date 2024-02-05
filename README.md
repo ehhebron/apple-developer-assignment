be73569e70cdffd340ef284660797cb29a271779
Hillary Hebron <hillaryhebron1@gmail.com>
2024-02-04 21:19:22 -0800

    simple navigation with current time from selected city

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# apple-developer-assignment
+# Apple Retail In-Store Developer Exercise 
+# Getting started 
+
+1. Checkout code: 
+1. Navigate inside the folder: 
+1. Install the node modules: `node install`
+1. Run the code in dev mode: `npm start` 
+
+### `npm start`
+
+Runs the app in development mode. 
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.
+Your app is ready to be deployed!
+
+### Deployment
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
+
+### `npm run build` fails to minify
+
+This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
---
+++ src/App.js
@@ -0,0 +1,126 @@
+import { useState, useEffect } from 'react';
+import debounce from 'lodash/debounce';
+import './App.scss';
+import navigation from './navigation.json';
+
+export default function App() {
+  const [selectedCity, setSelectedCity] = useState(navigation.cities[0].section);
+  const [tabWidth, setTabWidth] = useState('0px');
+  const [tabPosition, setTabPosition] = useState('0px');
+  const [animatedTransition, setAnimatedTransition] = useState(false);
+  const [time, setTime] = useState(null);
+  const [timezone, setTimezone] = useState(navigation.cities[0].timezone)
+
+  const handleNewCitySelection = (event, newSelectedCity) => {
+    // Check if there is no ongoing animation transition
+    if (animatedTransition === false) {
+      // If no ongoing animation, set animationTransition to true 
+      setAnimatedTransition(true);
+    }
+    //Get the position and dimensions of the clicked element 
+    const targetPosition = event.target.getBoundingClientRect();
+    // Set the selected city, timezone, width, and position
+    setSelectedCity(newSelectedCity.section);
+    setTimezone(newSelectedCity.timezone);
+    setTabWidth(targetPosition.width);
+    setTabPosition(targetPosition.x);
+  };
+
+  // handle the tab's width and position
+  const updateTab = () => {
+    const selectedCityElementPosition = document.getElementById(selectedCity).getBoundingClientRect();
+      setTabWidth(selectedCityElementPosition.width);
+      setTabPosition(selectedCityElementPosition.x);
+  };
+
+  const formatTime = (hour, minutes) => {
+    let ampm = "";
+  
+    if(hour === 0) {
+      hour = 12;
+      ampm = "AM";
+    } else if(hour === 12) {
+      ampm = "PM";
+    } else if(hour > 12) {
+      hour -= 12;
+      ampm = "PM";
+    } else {
+      ampm = "AM";
+    }
+  
+    if(hour.toString().length === 1) {
+      hour = `0${hour}`;
+    }
+    
+    if(minutes.toString().length === 1) {
+      minutes = `0${minutes}`;
+    }
+  
+    const formattedTime = `${hour}:${minutes} ${ampm}`;
+    return formattedTime;
+  }
+
+  // Fetches the current time based on the specified timezone from a world time API 
+  const updateLocalTime = () => {
+    fetch(`http://worldtimeapi.org/api/timezone/${timezone}/`)
+        .then((res) => res.json())
+        .then((json) => {
+            const time = json.datetime.split("T")[1].split(".")[0];
+            const [hour, minutes] = time.split(":");
+            const formattedTime = formatTime(hour, minutes);
+            setTime(formattedTime);
+        })
+        .catch((error) => console.error("Error fetching time:", error));
+  };
+
+  // Initialize component 
+  useEffect(() => {
+    updateTab();
+    updateLocalTime();
+  }, []);
+
+  // Handles window resize events with a debounce 
+  useEffect(() => {
+    const handleWindowResize = debounce(() => {
+      updateTab();
+    }, 500);
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  });
+
+  // Updates the local time when the timezone changes
+  useEffect(() => {
+    updateLocalTime();
+  }, [timezone]);
+
+
+  return (
+   <>
+    <section className="navigation">
+      <nav>
+        <ul>
+         {navigation.cities.map((city) => (
+            <li 
+              className={`${selectedCity === city.section ? "selectedCity" : ""}`}
+              id={city.section}
+              key={city.section}
+              onClick={(event) => handleNewCitySelection(event, city)}
+            >
+              {city.label}
+            </li>
+         ))} 
+        </ul>
+      </nav>
+      <div className={`tab${animatedTransition ? " transition" : ""}`}
+           style={{ width: tabWidth, left: tabPosition }}>
+      </div>
+    </section>
+    <section className="time">
+      <p>{time}</p>
+    </section>
+   </>
+  );
+}
